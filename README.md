d9f33abfb8d260fee3c1d714127e0ffce53bf00a
natan <natanmd604@gmail.com>
2026-09-06 16:19:44 -0300

    Add 16x16 sketching

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Etch-a-Sketch
+A project that's meant to better DOM manipulation skills
--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const container = document.getElementById("container");
+const MAXSIZE = 1024; // maximum size of the square grid in pixels
+container.style.width = "" + MAXSIZE + "px";
+container.style.height = "" + MAXSIZE + "px";
 
 for(i = 0; i < 16; i++){
     const gridLine = document.createElement("div");
@@ -7,7 +10,17 @@ for(i = 0; i < 16; i++){
     container.appendChild(gridLine);
     for(j = 0; j < 16; j++){
         const square = document.createElement("div");
+        const squareSize = 64;
+        square.style.width = "" + squareSize + "px";
+        square.style.height = "" + squareSize + "px";
         square.classList.add("square");
         gridLine.appendChild(square);
+        square.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "black";
+            // console.log(e);
+        });
     }
 }
+
+// n -> numero de squares
+// cada square precisa ter 1024/n pixels ao todo (borda 1px + dentro)
